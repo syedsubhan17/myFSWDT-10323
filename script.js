73bccae8b6ffa2e2484a75120b3ddac8c2773b2a
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-06 20:38:21 +0530

    start larning JS

---
+++ script.js
@@ -0,0 +1,11 @@
+const age = 123
+const Name = "this is. a string"
+const isAdult = true
+
+const names =['name1', 'name2', 'name3']
+
+//scope
+function greet(){
+    const name = 'me'
+}
+console.log(greet);
