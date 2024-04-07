346ba72b0256411d56259d288bf7a55f75d87c43
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-07 23:55:48 +0530

    temp commit

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function greet(){
     const name = 'me'
 }
 console.log(greet);
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        let result = 1;
+        for (let i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
+const number = 5;
+const result = factorial(number);
+console.log(`The factorial of ${number} is: ${result}`);
