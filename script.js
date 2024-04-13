ab5beeb2c9d4c509468e62d1446543ca8e3d4c7b
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2024-04-13 06:10:29 +0000

    talked about nested spreading

---
+++ script.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+=======
+const a = {
+    name: 'subhan',
+    age:22,
+    car:{
+        brand: 'BMW',
+        model: 'X5'
+    }
+}
+
+const b = {
+    ...a,
+}
+
+a.name = "syed"
+a.age = 18
+a.car.brand = 'Mercedes'
+a.car.model = 'C300D'
+console.log(a);
+>>>>>>> 24d56c9 (talked about nested spreading)
