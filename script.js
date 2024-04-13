44b5ac5588256692e655aa7bd76db94939837a48
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2024-04-13 05:52:23 +0000

    git commit intro to ES6 and it uses

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const a = {
-    name: 'subhan',
-    age:22,
-    car:{
-        brand: 'BMW',
-        model: 'X5'
-    }
-}
-
-const b = {}
-
-for(let key in a){
-    const vlue
-}
+const temp = 100;
+const temp = 100;
+const temp = 100;
+const temp = 100;
+const temp = 100;
