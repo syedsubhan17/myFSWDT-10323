1d9bbec133573c8db30011d6d2559960d6962b25
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-17 00:16:47 +0530

    querySelector - ( . ) and querySelectorAll - ( # )

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const headingObject = document.getElementById('heading')
-const listObject = document.getElementById('list')
+// const headingObject = document.getElementById('heading')
+// const listObject = document.getElementById('list')
 
-const listElement = document.getElementsByClassName('list-ele')
-console.log(listElement);
-
-listElement.forEach(element => { //dont use forEach for getElementsByClassName
-    console.log(element);    
-});
+// const heading = document.querySelector('.list-ele')
+const heading = document.querySelectorAll('#list-ele')
+console.log(heading);
