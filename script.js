5cfa0fc3515f8f173208a2f71484f58254a99818
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-16 18:24:54 +0530

    checked what is ternary operator

--- script.js
+++ script.js
@@ -4,13 +4,19 @@
 
 // greet('subhan', 30, 12)
 
-const greet = (...anything) => {
-    console.log(anything)
-}
+// const greet = (...anything) => {
+//     console.log(anything)
+// }
 
-const [name1, ...anything] = ['john', 'mario', 'Lightning']
+// const [name1, ...anything] = ['john', 'mario', 'Lightning']
 
-console.log(name1);
-console.log(anything);
+// console.log(name1);
+// console.log(anything);
 
-greet('subhan', 30, 12)
+// greet('subhan', 30, 12)
+
+//ternary Operator:
+
+const age = 19
+
+const res = age > 18 ? 'adult' : 'child'
