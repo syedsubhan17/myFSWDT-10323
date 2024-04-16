edf930764d28da0e22da78aa344e6e62c1f8c4ed
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-17 00:35:01 +0530

    used inbuilt toggle

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const body = document.querySelector('body')
 
 const toggleTheme = () =>{
-    if(body.classList.contains('dark')){
-        body.classList.remove('dark')
-        body.classList.add('light')
-    }
-    else{
-        body.classList.remove('light')
-        body.classList.add('dark')
-    }
+    body.classList.toggle('dark')
 }
