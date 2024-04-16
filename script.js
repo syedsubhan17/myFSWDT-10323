6df0033fd211de317b0937ff7e7ddf8ba4e45d94
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-17 00:08:34 +0530

    dont use forEach for getElementsByClassName

--- script.js
+++ script.js
@@ -2,20 +2,8 @@ const headingObject = document.getElementById('heading')
 const listObject = document.getElementById('list')
 
 const listElement = document.getElementsByClassName('list-ele')
+console.log(listElement);
 
-console.log(listElement); //getElementsbyClassname returns Array  eve nit conatins 1 value
-console.log(listObject); //it is normal
-
-
-
-// const arr = ['element1', 'element2', 'element3', 'element4', 'element5',]
-
-// const textNode = document.createTextNode('Hello World')
-
-// listObject.innerHTML = ' '
-// arr.forEach((element) => {
-//     const li = document.createElement('li')
-//     const text = document.createTextNode(element)
-//     li.appendChild(text)
-//     listObject.appendChild(li)
-// })
+listElement.forEach(element => { //dont use forEach for getElementsByClassName
+    console.log(element);    
+});
