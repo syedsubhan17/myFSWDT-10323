ca72c65c11f8b89c9b8c00759918c1570975f40a
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-16 19:39:26 +0530

    append child in DOM

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const headingObject = document.getElementById('heading')
-
-        headingObject.innerHTML = 'Hello World'
-        headingObject.style = 'color:orange'
+        const listObject = document.getElementById('list')
+        
+        const arr = ['element1', 'element2', 'element3', 'element4', 'element5',]
+        
+        arr.forEach((element) => {
+            const li = document.createElement('li')
+            li.innerHTML = element
+            listObject.appendChild(li)
+            console.log(li);
+        })
