a5c367f6e003b3f91e52bf5652cb8a0e25996f73
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-16 23:48:55 +0530

    element add before

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const headingObject = document.getElementById('heading')
-        const listObject = document.getElementById('list')
-        
-        const arr = ['element1', 'element2', 'element3', 'element4', 'element5',]
-        
-        arr.forEach((element) => {
-            const li = document.createElement('li')
-            li.innerHTML = element
-            listObject.appendChild(li)
-            console.log(li);
-        })
+const listObject = document.getElementById('list')
+
+const arr = ['element1', 'element2', 'element3', 'element4', 'element5',]
+
+const textNode = document.createTextNode('Hello World')
+
+arr.forEach((element) => {
+    const li = document.createElement('li')
+    const text = document.createTextNode(element)
+    li.appendChild(text)
+    listObject.appendChild(li)
+})
