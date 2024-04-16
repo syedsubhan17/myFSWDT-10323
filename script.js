fd61263c81de0ceedf75984f1c25c4bfb8895181
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-16 18:16:56 +0530

    unknown but used

--- script.js
+++ script.js
@@ -7,19 +7,9 @@ const a = {
     }
 }
 
-// const name = a.name
-// const age = a.age
-// const car = a.car
+const {name:name1, 
+    age:personAge, 
+    car:hteircar
+} = a
 
-// const {name, age, car} = a
- const {name, age, brand} = a     //brand will be undefine
-// console.log(name, car, age);
- console.log(name, age, brand);
-
-const arr = ['john', 'jane', 'copper']
-
-// const [name1,name2, name3] = arr
-const [name3,name2, name1] = arr
-
-// console.log(name1, name2, name3);
-console.log(name2, name3);
+console.log(name1, personAge, hteircar);
