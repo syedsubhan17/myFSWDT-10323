1d951adfaecba299c53c0b7be1cf5ee59bd602e0
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-17 00:22:11 +0530

    script tag in heading and use "defer" to load script faster

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // const headingObject = document.getElementById('heading')
 // const listObject = document.getElementById('list')
 
-// const heading = document.querySelector('.list-ele')
-const heading = document.querySelectorAll('#list-ele')
+const heading = document.querySelector('ul')
+// const heading = document.querySelectorAll('#list-ele')
 console.log(heading);
