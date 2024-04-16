fc1404ffc29ea8c708b82d51392e7016ea453d12
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-16 17:25:59 +0530

    spread operator in OBJECTS

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const a = [1, 2, 3 , 4, 5]
+// const a = [1, 2, 3 , 4, 5]
 
-const b = [...a]
-console.log(a);
-console.log(b); // used the spread operator
+// const b = [6, 7, 8, 9, 10]
+
+// const c = [...a,b]
+// console.log(c);
+
+const a = {
+    name: 'subhan',
+    age: 22
+}
+
+const b = {
+    ...a,
+}
+
+a.name = "syed"
+
+console.log(b);
