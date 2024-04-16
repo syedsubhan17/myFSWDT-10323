0a8c97e6a8b089119c6071ef7654589136cfe2a5
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-16 19:35:15 +0530

    basics of DOM

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 const headingObject = document.getElementById('heading')
-const listObject = document.getElementById('list')
 
-const arr = ['element1', 'element2', 'element3', 'element4', 'element5',]
-
-arr.forEach((element) => {
-    const li = document.createElement('li')
-    li.innerHTML = element
-    listObject.appendChild(li)
-    console.log(li);
-})
+        headingObject.innerHTML = 'Hello World'
+        headingObject.style = 'color:orange'
