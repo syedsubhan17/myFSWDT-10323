5020202e86b8748d5476cadf4a574882e956262b
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-17 16:04:35 +0530

    created colored click event

--- events.js
+++ events.js
@@ -3,4 +3,14 @@ const button = document.querySelector('button')
 
 body.addEventListener('click', (event) =>{
     console.log(`Your clicked on this position: ${event.clientX} ${event.clientY}`);
+    const ele = document.createElement('div')
+    ele.style =`
+    position: absolute`
+    ele.style.top = `${event.clientY}px`
+    ele.style.left = `${event.clientX}px`
+    ele.style.width = `10px`
+    ele.style.height = `10px`
+    ele.style.backgroundColor = `cyan`
+    ele.style.borderRadius = `50%`
+    body.appendChild(ele)
 })
