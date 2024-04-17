9c22cbebcd0c8726f1d2c4713134a571839357cf
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-18 03:10:36 +0530

    created a TODO List with ADD + DELETE + EDIT function

--- events.js
+++ events.js
@@ -1,50 +1,78 @@
-const todoConatainer = document.querySelector('ul')
-const input = document.querySelector('input')
-const button = document.querySelector('button')
+const todoConatainer = document.querySelector('ul');
+const input = document.querySelector('input');
+const addButton = document.querySelector('#add');
+const editButton = document.querySelector('#edit');
 
-const todos = []
+editButton.style = "display: none"
 
-const renderList = ()=>{
+let idOfElementToEdit = 'null';
+
+const todos = [];
+
+const showEdit = (id) => {
+    const element = todos.find((todo) => todo.id === id);
+    input.value = element.text
+    addButton.style = "display: none"
+    editButton.style = "display: inline"
+    idOfElementToEdit = id;
+}
+
+const renderList = () => {
     todoConatainer.innerHTML = '' //clear boox and add new item
-    todos.forEach((todo)=>{
+    todos.forEach((todo) => {
         const li = document.createElement('li')
-        const button = document.createElement('button')
-        button.addEventListener('click', (e)=>{
+        const deleteButton = document.createElement('button')
+        const editButton = document.createElement('button')
+
+        deleteButton.addEventListener('click', (e) => {
             li.remove()
             const id = todo.id //every element have it id or address location
             const index = todos.findIndex((todo) => todo.id === id)
-            todos.splice(index,1)
+            todos.splice(index, 1)
         }) //this will generate a delet button when add button hit
 
-        button.innerHTML = 'delete'
+        editButton.addEventListener('click', (e) => {
+            showEdit(todo.id) //for edit button function
+        })
+
+        editButton.style = "background: purple"
+
+        deleteButton.innerHTML = 'delete'
+        editButton.innerHTML = 'Edit'
         li.innerHTML = `${todo.text}`
-        li.appendChild(button) //add delet button with every task(li)
+        li.appendChild(deleteButton) //add delete1 button with every task(li)
+        li.appendChild(editButton) //add edit option button
         todoConatainer.appendChild(li)
     })
+}
 
+const handleAdd = (e) => {
+        const itemToAdd = input.value;
+        if (itemToAdd !== null && itemToAdd !== "") {//check the placeholder empty or not
+            todos.push({ //this will inser the user text
+                text: itemToAdd,
+                completed: false,
+                id: Date.now(),
+            });
+            input.value = "" //seprate one task from another and empty the placeholder for next task
+            renderList();
+            return
+        }
+        else {
+            return window.alert('adding an empty item is not possible') //placeholder is empty it send message to insert something
+        }
+};
+
+const handleEdit = (e) => {
+    const newvalue = input.value
+    const element = todos.find((todo) => todo.id === idOfElementToEdit)
+    element.text = newvalue
     console.log(todos);
+    renderList() //update after edit
+    addButton.style = "display: inline";
+    editButton.style = "display: none";
+    input.value = "";
 }
 
-
-const handleAdd = (e)=>{
-    // console.log('adding item')
-    // console.log(input.innerHTML);
-    // console.log(input.innerText);
-
-    const itemToAdd = input.value
-    if(itemToAdd !== null && itemToAdd !== ''){//check the placeholder empty or not
-        todos.push({ //this will inser the user text
-            text: itemToAdd,
-            completed: false,
-            id: Date.now()
-        })
-        renderList();
-        return
-    }
-    else{
-        return window.alert('adding an empty item is not possible') //placeholder is empty it send message to insert something
-    }
-    
-}
-
-button.addEventListener('click', handleAdd)
+addButton.addEventListener('click', handleAdd)
+editButton.addEventListener('click', handleEdit)
