a6b500696bb6ed1ee044de69aa714b1dd91c8495
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-20 00:24:28 +0530

    optimise the code for better execution

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+
+
 fetch('https://jsonplaceholder.typicode.com/todos')
+
 .then(data=>data.json())
-.then(finalData =>{
-    console.log(finalData);
-})
-.catch(err=>{
-    console.log(err);
+.then(result=>{
+    console.log(result);
 })
