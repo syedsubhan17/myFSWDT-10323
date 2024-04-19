4eb29e7abd9c06b96edbb202f602339d0e6cee95
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-20 00:22:38 +0530

    decrease no. of lines

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const response = fetch('https://jsonplaceholder.typicode.com/todos')
-
-response
-.then(data=>{
-    return data.json()
-})
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(data=>data.json())
 .then(finalData =>{
     console.log(finalData);
+})
+.catch(err=>{
+    console.log(err);
 })
