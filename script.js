4bbbc8cc464b7a773bb8b23f5803cecd79613983
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-20 00:12:51 +0530

    fetch data through link

--- script.js
+++ script.js
@@ -1,52 +1,8 @@
-// const getProducts1 = () => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             const products = [
-//                 { name: 'Apple', price: 100 },
-//                 { name: 'Orange', price: 100 },
-//                 { name: 'Banana', price: 100 },
-//                 { name: 'Grape', price: 100 },
-//                 { name: 'Pineapple', price: 100 },
-//                 { name: 'Mango', price: 100 },
-//                 { name: 'Strawberry', price: 100 },
-//                 { name: 'Watermelon', price: 100 },
-//                 { name: 'Peach', price: 100 },
-//                 { name: 'Cherry', price: 100 },
-//             ];
-//             resolve(products);
-//         }, 3000);
-//     });
-// };
+const response = fetch('https://jsonplaceholder.typicode.com/todos')
 
-// const syncFunction = () => {
-//     for (let i = 0; i < 10000; i++) {
-//         console.log('loop');
-//     }
-// };
-
-// const result = getProducts1()
-// result.then(phone =>{
-//     console.log(phone);
-// })
-
-// console.log(result);
-
-const age = 11
-// const age = 21
-
-
-const myPromise = new Promise((resolve,reject) => {
-    setTimeout(()=>{
-        if(age>18) resolve('You are eligible to vote')
-        else reject('You are not eligible to vote')
-    },3000)
-    
-})
-
-myPromise
-.then(result=>{
-    console.log(result);
-})
-.catch(error =>{
-    console.log(error);
+response.then(data=>{
+    const streamData = data.json()
+    streamData.then(finalData=>{
+        console.log(finalData);
+    })
 })
