81601badf6b7d5336ad72f96036d5703b492206f
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-19 12:35:47 +0530

    use of set interval and set timeout

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-// setInterval(()=>{
-//     console.log("Interval");//executing code every sec
-// },1000)
+// const getProducts1 = () =>{
 
-let i = 0;
+// }
 
-const id = setInterval(() =>{
-    console.log('intervel');
-    if(++i === 10){
-        clearInterval(id)
-    }
+
+
+
+let products
+
+const getProducts2 = () => {
+    setTimeout(()=>{
+        products = [
+            {name:'Apple', price: 100},
+            {name:'Orange', price: 100},
+            {name:'Banana', price: 100},
+            {name:'Grape', price: 100},
+            {name:'Pineapple', price: 100},
+            {name:'Mango', price: 100},
+            {name:'Strawberry', price: 100},
+            {name:'Watermelon', price: 100},
+            {name:'Peach', price: 100},
+            {name:'Cherry', price: 100},
+        ]
+    },3000)
+}
+
+getProducts2()
+
+console.log(products);
+
+setTimeout(() => {
+    console.log(products)
 },1000)
+setTimeout(() => {
+    console.log(products)
+},2000)
+setTimeout(() => {
+    console.log(products)
+},3000)
