e7c4d56eebaa2931adf012ea8b341496959d1955
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-19 11:10:59 +0530

    learnt syncronus task

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-// console.log('1');
-// for(let i = 0; i<10; i++){
-//     console.log('loop');
-// }
-// console.log('2');
 
-// setTimeout(()=>{
-//     console.log('executing');
-// },5000)
 
-for(let i = 0; i<1000; i++){
     console.log('one');
-}
+
 for(let i = 0; i<1000; i++){
     console.log('two');
 }
-for(let i = 0; i<1000; i++){
+
     console.log('three');
-}
