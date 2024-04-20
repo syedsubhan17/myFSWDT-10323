0392b6092432852f63887f329bd1716cfb4614e1
syedsubhan17 <ssubhan431401@gmail.com>
2024-04-20 18:49:52 +0530

    learnt use of "promise.all" in class

--- script.js
+++ script.js
@@ -11,17 +11,23 @@ const getTodos = async () =>{
     try{
         console.log('1');
 
-        await new Promise((resolve) => {
+        const p1 = new Promise((resolve) => {
             setTimeout(() => resolve(), 4000)
         })
+        // await p1
+        // console.log(2);
 
-        console.log(2);
+        // we want to run both of this at same time ===> soo we use "promise.all"
 
-        await new Promise((resolve) => {
+        const p2 = new Promise((resolve) => {
             setTimeout(()=>resolve(), 5000)            
         })
+        // await p2
+        // console.log(3); 
 
-        console.log(3);
+        const p3 = Promise.all([p1,p2])
+        await p3
+        console.log(p3);
     }
     catch (err){
         console.log(err);
